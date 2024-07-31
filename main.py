e274467cfc097091792d6b991fb91b333b039c08
hm <52304443+c29r3@users.noreply.github.com>
2024-07-31 20:11:32 +0300

    refactoring

--- main.py
+++ main.py
@@ -29,17 +29,14 @@ setup_logging(LOG_FILE, LOG_LEVEL)
 # Initialize bot
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN, parse_mode='Markdown')
 
-# Cache for the last fetched data
 last_fetched_data = {
     'solend_data': None,
     'kamino_data': None,
     'timestamp': None
 }
 
-# Cache for the last request time of each user
 user_last_request_time = {}
 
-# Moscow timezone
 moscow_tz = pytz.timezone('Europe/Moscow')
 
 
@@ -48,7 +45,7 @@ def send_telegram_message(message):
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.info("Message sent successfully.")
     except Exception as e:
-        logging.error(f"Failed to send message in send_telegram_message: {e}")
+        logging.error(f"Failed to send message: {e}")
 
 
 def get_kamino_data_with_retries(retries=3, delay=5):
@@ -58,53 +55,51 @@ def get_kamino_data_with_retries(retries=3, delay=5):
             logging.info(f"Data fetched successfully from Kamino on attempt {attempt + 1}.")
             return {'USDT': float(usdt_apy.strip('%')), 'USDC': float(usdc_apy.strip('%'))}
         except Exception as e:
-            logging.error(f"Attempt {attempt + 1} failed in get_kamino_data_with_retries: {e}")
+            logging.error(f"Attempt {attempt + 1} failed: {e}")
             if attempt < retries - 1:
                 time.sleep(delay)
-    logging.error("All retry attempts failed for Kamino in get_kamino_data_with_retries.")
+    logging.error("All retry attempts failed for Kamino.")
     return None
 
 
 def fetch_and_send_data():
     try:
-        # Get data from Solend
         solend_data = get_supply_apr()
         solend_data = {token: float(apr) for token, apr in solend_data.items()}
 
-        # Get data from Kamino with retries
         kamino_data = get_kamino_data_with_retries()
-        if kamino_data is None:
-            kamino_message = "Failed to fetch Kamino data."
-        else:
-            kamino_message = "\n".join([f'`{token} Supply APY: {apy:.3f}%`' for token, apy in kamino_data.items()])
+        kamino_message = "Failed to fetch Kamino data." if kamino_data is None else "\n".join(
+            [f'`{token} Supply APY: {apy:.3f}%`' for token, apy in kamino_data.items()]
+        )
 
-        # Cache the fetched data
         last_fetched_data['solend_data'] = solend_data
         last_fetched_data['kamino_data'] = kamino_data
         last_fetched_data['timestamp'] = datetime.now(moscow_tz)
 
-        # Check for conditions to send a message
-        message = ""
-        if any(apr <= MIN_APY_THRESHOLD for apr in solend_data.values()) or (
-                kamino_data and any(apy <= MIN_APY_THRESHOLD for apy in kamino_data.values())):
-            message = f"Solend Data:\n{solend_message(solend_data)}\n\nKamino Data:\n{kamino_message}\n\nLast updated: `{last_fetched_data['timestamp'].strftime('%Y-%m-%d %H:%M:%S %Z%z')}`"
+        if should_send_notification(solend_data, kamino_data):
+            message = f"Solend Data:\n{format_solend_message(solend_data)}\n\nKamino Data:\n{kamino_message}\n\nLast updated: `{last_fetched_data['timestamp'].strftime('%Y-%m-%d %H:%M:%S %Z%z')}`"
             send_telegram_message(message)
 
-        # Check for percentage change
-        if check_percentage_change(last_fetched_data['solend_data'], solend_data) or check_percentage_change(
+        if has_significant_change(last_fetched_data['solend_data'], solend_data) or has_significant_change(
                 last_fetched_data['kamino_data'], kamino_data):
-            message = f"ALERT: Significant APY change detected!\n\nSolend Data:\n{solend_message(solend_data)}\n\nKamino Data:\n{kamino_message}\n\nLast updated: `{last_fetched_data['timestamp'].strftime('%Y-%m-%d %H:%M:%S %Z%z')}`"
+            message = f"ALERT: Significant APY change detected!\n\nSolend Data:\n{format_solend_message(solend_data)}\n\nKamino Data:\n{kamino_message}\n\nLast updated: `{last_fetched_data['timestamp'].strftime('%Y-%m-%d %H:%M:%S %Z%z')}`"
             send_telegram_message(message)
 
     except Exception as e:
-        logging.error(f"Failed to fetch and send data in fetch_and_send_data: {e}")
+        logging.error(f"Failed to fetch and send data: {e}")
 
 
-def solend_message(data):
+def format_solend_message(data):
     return "\n".join([f'`{token}: {apr:.3f}%`' for token, apr in data.items()])
 
 
-def check_percentage_change(cached_data, new_data):
+def should_send_notification(solend_data, kamino_data):
+    return any(apr <= MIN_APY_THRESHOLD for apr in solend_data.values()) or (
+            kamino_data and any(apy <= MIN_APY_THRESHOLD for apy in kamino_data.values())
+    )
+
+
+def has_significant_change(cached_data, new_data):
     try:
         if cached_data is None:
             return False
@@ -115,7 +110,7 @@ def check_percentage_change(cached_data, new_data):
                     return True
         return False
     except Exception as e:
-        logging.error(f"Error in check_percentage_change: {e}")
+        logging.error(f"Error in has_significant_change: {e}")
         return False
 
 
@@ -142,7 +137,7 @@ def start(message):
         markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
         apy_btn = telebot.types.KeyboardButton('/apy')
         set_threshold_btn = telebot.types.KeyboardButton('/set_threshold')
-        set_percentage_change_btn = telebot.types.KeyboardButton('/set_percentage_change')  # Новый параметр
+        set_percentage_change_btn = telebot.types.KeyboardButton('/set_percentage_change')
         show_config_btn = telebot.types.KeyboardButton('/show_config')
         markup.add(apy_btn, set_threshold_btn, set_percentage_change_btn, show_config_btn)
 
@@ -164,17 +159,15 @@ def apy(message):
         if last_fetched_data['timestamp'] is None:
             bot.reply_to(message, "No data available yet. Please wait for the first fetch.")
         else:
-            # Use cached data
-            solend_message = "\n".join(
-                [f'`{token}: {apr:.3f}%`' for token, apr in last_fetched_data['solend_data'].items()])
+            solend_message = format_solend_message(last_fetched_data['solend_data'])
             kamino_message = "\n".join(
                 [f'`{token}: {apy:.3f}%`' for token, apy in last_fetched_data['kamino_data'].items()])
             timestamp = last_fetched_data['timestamp'].strftime('%Y-%m-%d %H:%M:%S %Z%z')
             message_text = f'Solend Data:\n{solend_message}\n\nKamino Data:\n{kamino_message}\n\nLast updated: `{timestamp}`'
             bot.reply_to(message, message_text)
     except Exception as e:
-        bot.reply_to(message, f"Failed to fetch data in apy: {e}")
-        logging.error(f"Failed to fetch data in apy: {e}")
+        bot.reply_to(message, f"Failed to fetch data: {e}")
+        logging.error(f"Failed to fetch data: {e}")
 
 
 @bot.message_handler(commands=['set_threshold'])
@@ -186,13 +179,9 @@ def set_threshold(message):
         logging.info(
             f"User {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name} ({message.from_user.id}) issued /set_threshold command")
         if is_authorized(message.from_user.id):
-            try:
-                msg = bot.reply_to(message,
-                                   f'Current APY threshold is `{MIN_APY_THRESHOLD}`. Please enter new APY threshold:')
-                bot.register_next_step_handler(msg, process_threshold_step)
-            except Exception as e:
-                bot.reply_to(message, f"Failed to set threshold in set_threshold: {e}")
-                logging.error(f"Failed to set threshold in set_threshold: {e}")
+            msg = bot.reply_to(message,
+                               f'Current APY threshold is `{MIN_APY_THRESHOLD}`. Please enter new APY threshold:')
+            bot.register_next_step_handler(msg, process_threshold_step)
         else:
             logging.warning(
                 f"Unauthorized user {message.from_user.username} ({message.from_user.id}) tried to set threshold")
@@ -207,24 +196,21 @@ def process_threshold_step(message):
         logging.info(
             f"User {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name} ({message.from_user.id}) issued new threshold: {message.text}")
         if is_authorized(message.from_user.id):
-            try:
-                new_threshold = float(message.text)
-                MIN_APY_THRESHOLD = new_threshold
-                bot.reply_to(message, f'APY threshold set to `{MIN_APY_THRESHOLD}`')
-            except ValueError:
-                bot.reply_to(message, 'Invalid value. Please enter a valid number.')
-            except Exception as e:
-                bot.reply_to(message, f"Failed to process threshold in process_threshold_step: {e}")
-                logging.error(f"Failed to process threshold in process_threshold_step: {e}")
+            new_threshold = float(message.text)
+            MIN_APY_THRESHOLD = new_threshold
+            bot.reply_to(message, f'APY threshold set to `{MIN_APY_THRESHOLD}`')
         else:
             logging.warning(
                 f"Unauthorized user {message.from_user.username} ({message.from_user.id}) tried to set threshold")
             bot.reply_to(message, 'You are not authorized to change settings.')
+    except ValueError:
+        bot.reply_to(message, 'Invalid value. Please enter a valid number.')
     except Exception as e:
-        logging.error(f"Error in process_threshold_step: {e}")
+        bot.reply_to(message, f"Failed to process threshold: {e}")
+        logging.error(f"Failed to process threshold: {e}")
 
 
-@bot.message_handler(commands=['set_percentage_change'])  # Новый хендлер
+@bot.message_handler(commands=['set_percentage_change'])
 def set_percentage_change(message):
     if is_request_too_frequent(message.from_user.id):
         return
@@ -233,13 +219,9 @@ def set_percentage_change(message):
         logging.info(
             f"User {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name} ({message.from_user.id}) issued /set_percentage_change command")
         if is_authorized(message.from_user.id):
-            try:
-                msg = bot.reply_to(message,
-                                   f'Current percentage change threshold is `{PERCENTAGE_CHANGE}`. Please enter new percentage change threshold:')
-                bot.register_next_step_handler(msg, process_percentage_change_step)
-            except Exception as e:
-                bot.reply_to(message, f"Failed to set percentage change threshold in set_percentage_change: {e}")
-                logging.error(f"Failed to set percentage change threshold in set_percentage_change: {e}")
+            msg = bot.reply_to(message,
+                               f'Current percentage change threshold is `{PERCENTAGE_CHANGE}`. Please enter new percentage change threshold:')
+            bot.register_next_step_handler(msg, process_percentage_change_step)
         else:
             logging.warning(
                 f"Unauthorized user {message.from_user.username} ({message.from_user.id}) tried to set percentage change threshold")
@@ -254,22 +236,18 @@ def process_percentage_change_step(message):
         logging.info(
             f"User {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name} ({message.from_user.id}) issued new percentage change threshold: {message.text}")
         if is_authorized(message.from_user.id):
-            try:
-                new_threshold = float(message.text)
-                PERCENTAGE_CHANGE = new_threshold
-                bot.reply_to(message, f'Percentage change threshold set to `{PERCENTAGE_CHANGE}`')
-            except ValueError:
-                bot.reply_to(message, 'Invalid value. Please enter a valid number.')
-            except Exception as e:
-                bot.reply_to(message,
-                             f"Failed to process percentage change threshold in process_percentage_change_step: {e}")
-                logging.error(f"Failed to process percentage change threshold in process_percentage_change_step: {e}")
+            new_threshold = float(message.text)
+            PERCENTAGE_CHANGE = new_threshold
+            bot.reply_to(message, f'Percentage change threshold set to `{PERCENTAGE_CHANGE}`')
         else:
             logging.warning(
                 f"Unauthorized user {message.from_user.username} ({message.from_user.id}) tried to set percentage change threshold")
             bot.reply_to(message, 'You are not authorized to change settings.')
+    except ValueError:
+        bot.reply_to(message, 'Invalid value. Please enter a valid number.')
     except Exception as e:
-        logging.error(f"Error in process_percentage_change_step: {e}")
+        bot.reply_to(message, f"Failed to process percentage change threshold: {e}")
+        logging.error(f"Failed to process percentage change threshold: {e}")
 
 
 @bot.message_handler(commands=['show_config'])
@@ -300,7 +278,6 @@ def show_config(message):
 def scheduled_fetch_and_send():
     while True:
         fetch_and_send_data()
-        # Wait for the specified interval
         time.sleep(CHECK_INTERVAL)
 
 
@@ -309,15 +286,12 @@ def run_bot():
         try:
             bot.polling(none_stop=True)
         except Exception as e:
-            logging.critical(f"Bot polling failed in run_bot: {e}", exc_info=True)
-            time.sleep(60)  # Wait before restarting bot polling
+            logging.critical(f"Bot polling failed: {e}", exc_info=True)
+            time.sleep(60)
 
 
 def main():
-    # Start the bot in a separate thread
     threading.Thread(target=run_bot).start()
-
-    # Schedule the fetch and send data task
     scheduled_fetch_and_send()
 
 
@@ -326,5 +300,5 @@ if __name__ == "__main__":
         try:
             main()
         except Exception as e:
-            logging.critical(f"Main loop exception in main: {e}", exc_info=True)
-            time.sleep(60)  # Wait before restarting the main loop
+            logging.critical(f"Main loop exception: {e}", exc_info=True)
+            time.sleep(60)
